913d8efd57ffe33422567e605a7c2a807a0ce770
Jamie Sandell <jamie.sandell@outlook.com>
2021-03-22 07:59:03 +0000

    Working on fold.c

--- The C Programming Language Second Edition/Chapter 01/Exercise 1-22 - FoldLongInput/fold.c
+++ The C Programming Language Second Edition/Chapter 01/Exercise 1-22 - FoldLongInput/fold.c
@@ -51,44 +51,99 @@ void fold_input(char input[], int input_length)
 
     int line_char_count = 0;
     int word_char_count = 0;
+    int line_word_count = 0;
     int word_start = 0;
     int word_end = 0;
     int last_blank_pos = -1;
     char prev_char = ' ';
     char curr_char = ' ';
-    for (int curr_pos = 0; curr_pos < input_length; ++curr_pos)
+    for (int curr_pos = 0; curr_pos <= input_length; ++curr_pos)
     {
+        // TODO work with tabs
         ++line_char_count;
-        ++word_char_count;
-
         curr_char = input[curr_pos];
-        if (word_char_count == 1)
+
+        if (word_char_count == 0) // Found the start of a word. Potentially starts with whitespace, but that's ok
         {
             word_start = curr_pos;
+            ++word_char_count;
         }
-        else if ((curr_char == ' ' && prev_char != ' ') || (curr_pos == input_length - 1) || (line_char_count == MAX_LINE_WIDTH))
+        else
         {
-            word_end = curr_pos;            
-            if (line_char_count < MAX_LINE_WIDTH)
+            word_char_count++;
+        }
+        // Have we found the end of a word?
+        // Also make sure we are less than the line width, otherwise we'd overflow it
+        if (curr_char == ' ' && prev_char != ' ' && line_char_count < MAX_LINE_WIDTH)
+        {
+            ++word_char_count;
+            line_word_count++;
+            word_end = curr_pos; // want to print the word including the space at the end
+            print_word(input, word_start, word_end);
+            word_char_count = word_start = word_end = 0; // Reset for the next word
+        }
+        // Have we reached the end of the line?
+        else if (line_char_count == MAX_LINE_WIDTH)
+        {
+            // peek at the next character if there's more input available
+            if (curr_pos < input_length)
             {
-                print_word(input, word_start, word_end);
-                putchar(curr_char);
+                if (input[curr_pos+1] == ' ') // end of the word
+                {
+                    ++word_char_count;
+                    word_end = curr_pos;
+                    print_word(input, word_start, word_end);
+                    word_char_count = word_start = word_end = 0;
+                    curr_pos++; // the next character is the space before the next word, so want to move forward for the next iteration in the loop
+                }
+                else if(input[curr_pos] != ' ' && input[curr_pos + 1] == ' ')
+                {
+                    ++word_char_count;
+                    word_end = curr_pos;
+                    print_word(input, word_start, word_end);
+                    word_char_count = word_start = word_end = 0;
+                    curr_pos++; // the next character is the space before the next word, so want to move forward for the next iteration in the loop
+                }
+                else if(input[curr_pos] == ' ')
+                {
+                    ++word_char_count;
+                    word_end = curr_pos;
+                    print_word(input, word_start, word_end);
+                    word_char_count = word_start = word_end = 0;
+                }
+                else // do something intelligent with the line now that it has reached the max line width without it being a complete word
+                {
+                    if(line_word_count == 0)
+                    {
+                        // 0 complete words this line so print the line as is
+                        word_char_count++;
+                        word_end = curr_pos;
+                        print_word(input, word_start, word_end);
+                        word_char_count = word_start = word_end = 0;
+                    }
+                    else
+                    {
+                        // at least one complete word this line, move the start of the current word (-1 for the loop increment) so it starts on the next line
+                        curr_pos = word_start - 1;
+                        line_word_count = 0;
+                        word_char_count = 0;
+                        word_start = 0;
+                        word_end = 0;
+                    }
+                }
+                putchar('\n'); // no more characters allowed this line so move down to the next
             }
-            else
-            {
-                line_char_count = 0;
-            }
-            word_char_count = 0;
+            line_word_count = line_char_count = 0; // reset for the next line
         }
 
         prev_char = curr_char;
     }
-    putchar('\n');
+    //putchar('\n'); // terminate the line
 }
 
 void print_word(char word[], int word_start, int word_end)
 {
-    for (int i = word_start; i < word_end; ++i)
+    for (int i = word_start; i <= word_end; ++i)
     {
         putchar(word[i]);
     }
