ce41449e0e905f1bcaff96d6b8b9ee30cd5e4276
Jamie Sandell <jamie.sandell@outlook.com>
2021-03-29 13:42:41 +0100

    Started Exercise 1-23

---
+++ The C Programming Language Second Edition/Chapter 01/Exercise 1-23 - RemoveAllComments/RemoveComments.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+int MAX_INPUT = 1000;
+
+void get_input(char output[], int output_length);
+
+int main()
+{
+    return 0;
+}
+
+/* Reads input until EoF or output_length reached and stores it in output */
+void get_input(char output[], int output_length)
+{
+    int c;
+    for (int i = 0; i < output_length; i++)
+    {
+        
+    }
+}
