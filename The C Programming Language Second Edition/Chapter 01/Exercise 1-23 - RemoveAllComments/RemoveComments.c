bc7d4d2a4b1077acfafd54de207c9d55989294c8
jamiesandell <jamie.sandell@outlook.com>
2021-04-03 18:24:39 +0100

    Exercise 1-23

--- The C Programming Language Second Edition/Chapter 01/Exercise 1-23 - RemoveAllComments/RemoveComments.c
+++ The C Programming Language Second Edition/Chapter 01/Exercise 1-23 - RemoveAllComments/RemoveComments.c
@@ -4,75 +4,66 @@ C comments do not nest. */
 
 #include "stdio.h"
 
-#define MAX_INPUT 5
-
-typedef int bool;
-#define TRUE 1
-#define FALSE 0
-
-int get_input(char output[], int output_length);
-int strip_comments(char input[], int input_length, char output[], int output_length);
-
+#define PROGRAM         0
+#define BEGIN_COMMENT   1
+#define COMMENT         2
+#define END_COMMENT     3
+#define QUOTE           4
 
 int main()
 {
-    char input[MAX_INPUT + 1]; /* +1 for the null termination */
-    int input_length = get_input(input, MAX_INPUT + 1);
-    char output[input_length];
-    int output_length = strip_comments(input, input_length, output, input_length); /* At this point the length of output is input_length */
+    int this_char, quote_char, state;
+    state = PROGRAM;
+
+    while((this_char = getchar()) != EOF)
+    {
+        if (state == PROGRAM)
+        {
+            if (this_char == '/')
+            {
+                state = BEGIN_COMMENT;
+            }
+            else if(this_char == '\'' || this_char == '"')
+            {
+                state = QUOTE;
+                quote_char = this_char; /* single variable to handle both types of quote, ' or " */
+            }
+            else
+            {
+                putchar(this_char);
+            }
+        }
+        else if (state == BEGIN_COMMENT)
+        {
+            if (this_char == '*')
+            {
+                state = COMMENT;
+            }
+            else
+            {
+                putchar('/'); /* Potentially had a comment, but it turns out it was a slash followed by a non-* character */
+                state = PROGRAM;
+            }
+        }
+        else if(state == COMMENT)
+        {
+            if (this_char == '*')
+            {
+                state = END_COMMENT;
+            }
+        }
+        else if (state == END_COMMENT)
+        {
+            if (this_char == '/')
+            {
+                state = PROGRAM;
+            }
+            else if(this_char != '*')
+            {
+                state = COMMENT; /* Still in a comment, but we can't be end of the comment as we need a * before a / */
+            }
+        }
+    }
+
     return 0;
-}
-
-/* Reads input until EoF or output_length reached, and stores it in output.
-Output is terminated as a null terminated string.
-Returns the size (null termination position) of output. */
-int get_input(char output[], int output_length)
-{
-    int c;
-    int i;
-    for (i = 0; i < output_length; i++)
-    {
-        c = getchar();
-        if (c == EOF)
-        {
-            break;
-        }
-        else
-        {
-            output[i] = c;
-        }
-    }
-    output[i] = '\0';
-    return i;
-}
-
-/* Strips the comments from the input and puts the result in output.
-input_length and output_length should be inclusive of the null termination character.
-Returns the size (null termination position) of output. */
-int strip_comments(char input[], int input_length, char output[], int output_length)
-{
-    int output_pos = 0;
-    char prev_char = ' ';
-    char curr_char;
-    bool in_comment = FALSE;
-    for (int i = 0; i < input_length - 1; i++)
-    {
-        curr_char = input[i];
-        if (in_comment == FALSE)
-        {
-            if ((prev_char == '/') && (curr_char == '*'))
-            {
-                in_comment = TRUE;
-            }
-        }
-        else
-        {
-            if ((prev_char == '*') && (curr_char == '/'))
-            {
-                in_comment = FALSE;
-            }
-        }
-        prev_char = curr_char;
-    }
-    return output_pos;
 }
