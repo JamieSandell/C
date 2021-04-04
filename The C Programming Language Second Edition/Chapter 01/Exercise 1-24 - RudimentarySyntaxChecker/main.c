259a85da4e96ebe8c904d7825d4adb06f3e5d2d7
jamiesandell <jamie.sandell@outlook.com>
2021-04-04 07:29:31 +0100

    Finished Exercise 1-24

--- The C Programming Language Second Edition/Chapter 01/Exercise 1-24 - RudimentarySyntaxChecker/main.c
+++ The C Programming Language Second Edition/Chapter 01/Exercise 1-24 - RudimentarySyntaxChecker/main.c
@@ -14,8 +14,8 @@ program is hard if you do it with full generality.) */
 int main()
 {
     int state, this_char, quote_char;
-    int parantheses, brackets, braces;
-    parantheses = braces = braces = 0;
+    int left_parantheses, left_brackets, left_braces, right_parantheses, right_brackets, right_braces;
+    left_parantheses = left_braces = left_brackets = right_parantheses = right_braces = right_brackets = 0;
 
     state = CODE;
     while((this_char = getchar()) != EOF)
@@ -23,19 +23,38 @@ int main()
         if (state == CODE)
         {
             if (this_char == '/') { state == START_COMMENT; }
-            else if(this_char == '(') { parantheses++; }
-            else if(this_char == '[') { brackets++; }
-            else if(this_char == '{') { braces++; }
+            if (this_char == '\'' || this_char == '"')
+            {
+                state == QUOTE;
+                quote_char = this_char;
+            }
+            else if(this_char == '(') { left_parantheses++; }
+            else if(this_char == '[') { left_brackets++; }
+            else if(this_char == '{') { left_braces++; }
+            else if(this_char == ')') { right_parantheses++; }
+            else if(this_char == ']') { right_brackets++; }
+            else if(this_char == '}') { right_braces++; }
         }
         else if(state == START_COMMENT)
         {
             if (this_char == '*') { state = COMMENT; }
+            state == CODE;
         }
         else if(state == COMMENT)
         {
-            if (this_char == '/') { state == END_COMMENT; }
+            if (this_char == '*') { state == END_COMMENT; }
+        }
+        else if(state == END_COMMENT)
+        {
+            if (this_char == '/') { state = CODE; }
+            else if(this_char != '*') { state = COMMENT; } /* stay in END_COMMENT if input looked like /***** for example, otherwise back to COMMENT */
         }
     }
+
+    if (left_braces != right_braces) { printf("Error: Left brace and right brace mismatch.\n"); }
+    else if(left_brackets != right_brackets) { printf("Error: Left brackets and right brackets mismatch.\n"); }
+    else if(left_parantheses != right_parantheses) { printf("Error: Left parantheses and right parantheses mismatch.\n"); }
+    else { printf("Success.\n"); }
 
     return 0;
 }
