b508aa70796ae7b81ce5db7fea4cbd23616638da
jamiesandell <jamie.sandell@outlook.com>
2021-04-05 13:45:26 +0100

    Exercise 2-3

--- The C Programming Language Second Edition/Chapter 02/Exercise 2-3 - HexToInt/main.c
+++ The C Programming Language Second Edition/Chapter 02/Exercise 2-3 - HexToInt/main.c
@@ -10,23 +10,45 @@ int upper(int c);
 
 int main()
 {
+    char fifteen[] = "0XF";
+    char fourty_two[] = "2A";
+    char hundred_thousand[] = "186A0";
+
+    printf("%i\n", htoi(fifteen));
+    printf("%i\n", htoi(fourty_two));
+    printf("%i\n", htoi(hundred_thousand));
+
     return 0;
 }
 
+/* Convertx a hex string to an integer. Ignores invalid characters. */
 int htoi(char hex[])
 {
     int first = upper(hex[0]);
     int second = upper(hex[1]);
     int pos = 0;
+    int num = 0;
+    char hex_map[] = {"0123456789ABCDEF"};
+
     if (first == '0' && second == 'X') /* Optional 0X used? */
     {
         pos = 2;
     }
 
-    for (int i = pos; i != '\0'; i++)
+    /* Cycle each character of the input, then make sure it's a valid hex character. */
+    for (int i = pos; hex[i] != '\0'; i++)
     {
-        
+        for (int j = 0; j < BASE; j++)
+        {
+            if (hex[i] == hex_map[j])
+            {
+                num = num * BASE + j;
+                break;
+            }
+        }
     }
+
+    return num;
 }
 
 /* Convert lower case ASCII to upper case ASCII */
