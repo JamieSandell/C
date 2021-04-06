ca101ee9aeeb5cb337bb437a4629417b02d36527
jamiesandell <jamie.sandell@outlook.com>
2021-04-06 07:28:36 +0100

    Exercise 2-6

---
+++ The C Programming Language Second Edition/Chapter 02/Exercise 2-6 - GetBits/main.c
@@ -0,0 +1,20 @@
+/* Write a function setbits(x,p,n,y) that returns x with the n
+bits that begin at position p set to the rightmost n bits of y, leaving the other
+bits unchanged. 
+https://stackoverflow.com/questions/15698619/what-function-is-kr-exercise-2-6-really-asking-for
+https://stackoverflow.com/questions/1415854/kr-c-exercise-help
+https://clc-wiki.net/wiki/K&R2_solutions:Chapter_2:Exercise_6 */
+
+#include <stdio.h>
+
+int setbits(unsigned x, int p, int n, int y);
+
+int main()
+{
+    return 0;
+}
+
+int setbits(unsigned x, int p, int n, int y)
+{
+
+}
