6d3f0cf816b943e769e79130240667825a5f9ca4
jamiesandell <jamie.sandell@outlook.com>
2021-04-11 16:58:39 +0100

    Exercise 2-8

---
+++ The C Programming Language Second Edition/Chapter 02/Exercise 2-8 - RightRot/main.c
@@ -0,0 +1,15 @@
+/* Write a function rightrot(x,n) that returns the value of the integer x rotated to the right by n bit positions. */
+
+#include <stdio.h>
+
+unsigned rightrot(unsigned x, int n);
+
+int main()
+{
+    return 0;
+}
+
+unsigned rightrot(unsigned x, int n)
+{
+
+}
