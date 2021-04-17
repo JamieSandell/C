7ae524b9a0838d015c719957251cfbf015ad5484
Jamie Sandell <jamie.sandell@outlook.com>
2021-04-17 18:24:06 +0100

    Exercise 3-2

---
+++ The C Programming Language Second Edition/Chapter 03/Exercise 3-2 - Escape/main.c
@@ -0,0 +1,13 @@
+/* Exercise 3-2. Write a function escape(s,t) that converts characters like
+newline and tab into visible escape sequences like \n and \t as it copies the
+string t to s. Use a switch. Write a function for the other direction as well,
+converting escape sequences into the real characters. */
+
+#include <stdio.h>
+
+
+
+int main()
+{
+    return 0;
+}
