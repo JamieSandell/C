e67c9d9b7ae0e8c647104dd74e179be5d32eff7b
jamiesandell <jamie.sandell@outlook.com>
2021-04-22 08:15:27 +0100

    Exercise 3-2

--- The C Programming Language Second Edition/Chapter 03/Exercise 3-2 - Escape/main.c
+++ The C Programming Language Second Edition/Chapter 03/Exercise 3-2 - Escape/main.c
@@ -5,9 +5,29 @@ converting escape sequences into the real characters. */
 
 #include <stdio.h>
 
-
+void escape(char s[], char t[]);
+void escape_reverse(char s[], char t[]);
 
 int main()
 {
     return 0;
+}
+
+/* Expects null terminated strings to be passed in. */
+void escape(char s[], char t[])
+{
+    for (int i = 0; t[i] != '\0'; i++)
+    {
+        switch (t[i])
+        {
+            case '\n':
+                break;
+            case '\t':
+                break;
+        }
+    }
+}
+void escape_reverse(char s[], char t[])
+{
+
 }
