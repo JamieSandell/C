a578f8b35baac0dd1bc18b27d931f458e717196e
jamiesandell <jamie.sandell@outlook.com>
2021-04-27 08:20:44 +0100

    Exercise 3-5

---
+++ The C Programming Language Second Edition/Chapter 03/Exercise 3-5 - itob/main.c
@@ -0,0 +1,10 @@
+/* Write the function itob(n,s,b) that converts the interger n
+into a base b character representation in the string s. In particular,
+itob(n,s,16) formats n as a hexadecimal integer in s. */
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
