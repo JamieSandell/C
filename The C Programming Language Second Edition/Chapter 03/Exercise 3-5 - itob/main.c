35e44223e188b294122e888b5a51b97cde7f3eec
jamiesandell <jamie.sandell@outlook.com>
2021-04-27 13:36:34 +0100

    Exercise 3-5

--- The C Programming Language Second Edition/Chapter 03/Exercise 3-5 - itob/main.c
+++ The C Programming Language Second Edition/Chapter 03/Exercise 3-5 - itob/main.c
@@ -3,8 +3,23 @@ into a base b character representation in the string s. In particular,
 itob(n,s,16) formats n as a hexadecimal integer in s. */
 
 #include <stdio.h>
+#include <string.h>
+
+void reverse(char s[]);
 
 int main()
 {
     return 0;
+}
+
+/* Reverse s in place */
+void reverse(char s[])
+{
+    int i, j, c;
+    for(i = 0, j = strlen(s) - 1; j > i; --j)
+    {
+        c = s[i];
+        s[i++] = s[j];
+        s[j] = c;
+    }
 }
