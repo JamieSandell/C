378fd24f26c5a0fa3c4deef71b6c741485a322e3
jamiesandell <jamie.sandell@outlook.com>
2021-04-27 07:08:31 +0100

    Exercise 3-4

--- The C Programming Language Second Edition/Chapter 03/Exercise 3-4 - Itoa/main.c
+++ The C Programming Language Second Edition/Chapter 03/Exercise 3-4 - Itoa/main.c
@@ -27,11 +27,50 @@ void itoa(int n, char s[])
 -128 
 
 however the biggest positive number we can store is 2 to the power of n-1 - 1 = 127.
-So at  */
+So if we passed in -128, on the "make n positive" line, it can't store +128, so it becomes 0.  */
 
 #include <stdio.h>
+#include <string.h>
+
+void itoa(int n, char s[]);
+void reverse(char s[]);
 
 int main()
 {
+    char s[1000];
+    int number = -128;
+    itoa(number, s);
+    printf("Converted %i to a string: %s\n", number, s);
     return 0;
+}
+
+void itoa(int n, char s[])
+{
+    int i = 0;
+    int sign = (n < 0) ? -1 : 1;
+
+    do
+    {
+        s[i++] = sign * (n % 10) + '0';
+    } while ((n /= 10) != 0);
+    if (sign < 0)
+    {
+        s[i++] = '-';
+    }
+    s[i] = '\0';
+    reverse(s);
+    
+}
+
+/* From page 62.
+Reverse string s in place. */
+void reverse(char s[])
+{
+    int c, i, j;
+    for(i = 0, j = strlen(s) - 1; i < j; i++, j--)
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
 }
