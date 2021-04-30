2d72fc742ebfb93040c77be94354aa2e9d33334c
jamiesandell <jamie.sandell@outlook.com>
2021-04-30 13:37:18 +0100

    Exercise 4-2

--- The C Programming Language Second Edition/Chapter 04/Exercise 4-2 - atof/main.c
+++ The C Programming Language Second Edition/Chapter 04/Exercise 4-2 - atof/main.c
@@ -19,16 +19,57 @@ double atof(char s[])
     {
         i++;
     }
-    for (val = 0.0; isdigit(s[i])); i++)
+    for (val = 0.0; isdigit(s[i]); i++)
     {
-        val = 10.0 * val + (s[i] - '0);
+        val = 10.0 * val + (s[i] - '0');
     }
     if (s[i] == '.')
     {
         i++;
     }
-    for (power = 1.0; isdigit(s[i])); i++)
+    for (power = 1.0; isdigit(s[i]); i++)
     {
-        
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
     }
+    return sign * val / power
 } */
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+double atof(char s[]);
+
+int main()
+{
+
+}
+
+double atof(char s[])
+{
+    double val, power;
+    int i, sign;
+
+    for (i = 0; isspace(s[i])); i++)
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+    {
+        i++;
+    }
+    for (val = 0.0; isdigit(s[i]); i++)
+    {
+        val = 10.0 * val + (s[i] - '0');
+    }
+    if (s[i] == '.')
+    {
+        i++;
+    }
+    for (power = 1.0; isdigit(s[i]); i++)
+    {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    return sign * val / power
+}
