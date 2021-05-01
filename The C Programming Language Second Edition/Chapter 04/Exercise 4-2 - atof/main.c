8e7f0a0d0e719e6367d0dd08ada889f13c656771
jamiesandell <jamie.sandell@outlook.com>
2021-05-01 08:49:25 +0100

    Exercise 4-2

--- The C Programming Language Second Edition/Chapter 04/Exercise 4-2 - atof/main.c
+++ The C Programming Language Second Edition/Chapter 04/Exercise 4-2 - atof/main.c
@@ -12,7 +12,7 @@ double atof(char s[])
     double val, power;
     int i, sign;
 
-    for (i = 0; isspace(s[i])); i++)
+    for (i = 0; isspace(s[i]); i++)
         ; # skip white space
     sign = (s[i] == '-') ? -1 : 1;
     if (s[i] == '+' || s[i] == '-')
@@ -40,18 +40,20 @@ double atof(char s[])
 #include <string.h>
 
 double atof(char s[]);
+double power(double x, double y);
 
 int main()
 {
-
+    char number[] = {"10e2"};
+    printf("%s converted to double: %f\n", number, atof(number));
 }
 
 double atof(char s[])
 {
-    double val, power, exponent, exponent_power;
+    double val, val_power, exponent, exponent_power;
     int i, sign, exponent_sign;
 
-    for (i = 0; isspace(s[i])); i++)
+    for (i = 0; isspace(s[i]); i++)
         ;
     sign = (s[i] == '-') ? -1 : 1;
     if (s[i] == '+' || s[i] == '-')
@@ -66,14 +68,14 @@ double atof(char s[])
     {
         i++;
     }
-    for (power = 1.0; isdigit(s[i]); i++)
+    for (val_power = 1.0; isdigit(s[i]); i++)
     {
         val = 10.0 * val + (s[i] - '0');
-        power *= 10.0;
+        val_power *= 10.0;
     }
     if (s[i] == 'e' || s[i] == 'E')
     {
-        exponent_sign = (s[i] == '-') ? -1 : 1;
+        exponent_sign = (s[++i] == '-') ? -1 : 1;
         if (s[i] == '-' || s[i] == '+')
         {
             s++;
@@ -86,10 +88,29 @@ double atof(char s[])
         {
             i++;
         }
-        for (power = 1.0; isdigit(s[i]); i++)
+        for (exponent_power = 1.0; isdigit(s[i]); i++)
         {
-            exponent = 
+            exponent = 10.0 * exponent + (s[i] - '0');
+            exponent_power *= 10.0;
         }
+        return power(sign * val / val_power, exponent_sign * exponent / exponent_power);
     }
-    return sign * val / power
+    return sign * val / val_power;
+}
+
+double power(double x, double y)
+{
+    double result = x;
+    int i;
+
+    if (y)
+    {
+        for (i = 0; i < y - 1; i++)
+        {
+            result *= x;
+        }
+        return result;
+    }    
+
+    return result;
 }
