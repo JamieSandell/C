bfbfa7f04b106e776911ae5e1a835864937a0315
jamiesandell <jamie.sandell@outlook.com>
2021-05-20 13:47:37 +0100

    Exercise 5-4

--- The C Programming Language Second Edition/Chapter 05/Exercise 5-4 - strend/main.c
+++ The C Programming Language Second Edition/Chapter 05/Exercise 5-4 - strend/main.c
@@ -7,7 +7,7 @@ int strend(char *s, char *t);
 
 int main()
 {
-    char string1[] = "Jamie";
+    char string1[] = "Bob Jamie";
     char string2[] = "Jamie";
 
     int result = strend(string1, string2);
@@ -19,11 +19,12 @@ int main()
 int strend(char *s, char *t)
 {
     int s_char_count = 0;
-    while(*s)
+    while(*s) /* count all the characters in the string, not including the null terminator */
     {
         s_char_count++;
         s++;
     }
+    s--; /* Point s to the last character in the string, not the null terminator */
 
     int t_char_count = 0;
     while(*t)
@@ -31,16 +32,18 @@ int strend(char *s, char *t)
         t_char_count++;
         t++;
     }
+    t--;
 
     if (t_char_count > s_char_count)
     {
+        /* If there are more characters in t than s, then t can't be at the end of s. */
         return 0;
     }
 
     int s_offset, t_offset;
+    /* Loop backwards through the strings, if the character is equal keep going, count up the offsets each time */
     for(s_offset = 0, t_offset = 0; *(s - s_offset) == *(t - t_offset); s_offset++, t_offset++)
-    {
-        printf("\n");
-    }
-    return ((t_char_count - t_offset) == 0) ? 1 : 0;
+        ;
+    return ((t_char_count - t_offset) == 0) ? 1 : 0; /* if the number of characters in t - t_offset == 0
+    (t_offset was incremented each time a matching character was found going backwards in both strings), then we have a match */
 }
