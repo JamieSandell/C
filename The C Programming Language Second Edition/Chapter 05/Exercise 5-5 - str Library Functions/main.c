f8fea20431c9e6749e9d5e3f2a421920205e7a4f
jamiesandell <jamie.sandell@outlook.com>
2021-05-22 06:43:36 +0100

    Exercise 5-5

--- The C Programming Language Second Edition/Chapter 05/Exercise 5-5 - str Library Functions/main.c
+++ The C Programming Language Second Edition/Chapter 05/Exercise 5-5 - str Library Functions/main.c
@@ -10,6 +10,10 @@ char *strncpy(char *s, const char *t, size_t n);
 
 int main()
 {
+    char name[] = "Jamie";
+    char copy_to[10];
+    strncpy(copy_to, name, 10);
+
     return 0;
 }
 
@@ -19,6 +23,7 @@ size_t strlen(const char *s)
     size_t length = 0;
     while(*s)
     {
+        s++;
         length++;
     }
     return length;
@@ -28,23 +33,20 @@ size_t strlen(const char *s)
 Pad with '\0's if t has fewer than n characters. */
 char *strncpy(char *s, const char *t, size_t n)
 {
-    size_t t_length = strlen(s);
-    if (n > t_length)
-    {
-        printf("Error: %i is more characters than '%s' has\n", t_length, t);
-        return s;
-    }
-
+    size_t t_length = strlen(t);
     size_t i;
+    /* Copy up to n-chars or up to t_length if n-chars is greater than t_length*/
     for (i = 0; i < n && i < t_length; i++)
     {
         *(s + i) = *(t + i);
     }
-    for (; i < t_length; i++)
+    *(s + i) = '\0'; /* null terminate here, if we do it after the next loop we'd go out of bounds */
+    i++;
+    /* If chars copied is less than number of chars to copy, pad the rest with '\0's */
+    for (; i < n; i++)
     {
         *(s + i) = '\0';
-    }
-    *s - '\0';
+    }    
 
     return s;
 }
