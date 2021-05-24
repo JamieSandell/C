e8ec323a67b1e4c7589dd8b8685cd0a2d7411cfe
Jamie Sandell <jamies@lan.cyclops-electronics.com>
2021-05-24 13:37:57 +0100

    Exercise 5-7

--- The C Programming Language Second Edition/Chapter 05/Exercise 5-7 - Rewrite/main.c
+++ The C Programming Language Second Edition/Chapter 05/Exercise 5-7 - Rewrite/main.c
@@ -16,6 +16,7 @@ int readlines(char *line_ptr[], int max_lines);
 void writelines(char *line_ptr[], int nlines);
 
 void qsort(char *lineptr[], int left, int right);
+void swap(char *v[], int i, int j);
 
 /* sort input lines */
 int main()
@@ -111,8 +112,42 @@ int readlines(char *line_ptr[], int max_lines)
 /* writelines: write output lines */
 void writelines(char *line_ptr[], int nlines)
 {
-    while(--nlines > 0)
+    while(nlines-- > 0)
     {
         printf("%s\n", *line_ptr++);
     }
+}
+
+/* qsort: sort v[left]...v[right] into increasing order */
+void qsort(char *v[], int left, int right)
+{
+    int i, last;
+    void swap(char *v[], int i, int j);
+
+    if (left >= right) /* do nothing if array contains fewer than two elements */
+    {
+        return;
+    }
+    swap(v, left, (left + right) / 2);
+    last = left;
+    for (i = left + 1; i<= right; i++)
+    {
+        if (strcmp(v[i], v[left]) < 0)
+        {
+            swap(v, ++last, i);
+        }
+    }
+    swap(v, left, last);
+    qsort(v, left, last - 1);
+    qsort(v, last + 1, right);
+}
+
+/* swap: interchange v[i] and v[j] */
+void swap(char *v[], int i, int j)
+{
+    char *temp;
+
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
 }
