5d775cdf2f0f20cab298958af8fa09d52ba8f2a2
jamiesandell <jamie.sandell@outlook.com>
2021-05-27 08:41:05 +0100

    Exercise 5-9

--- The C Programming Language Second Edition/Chapter 05/Exercise 5-9 - Rewrite/main.c
+++ The C Programming Language Second Edition/Chapter 05/Exercise 5-9 - Rewrite/main.c
@@ -3,11 +3,10 @@ pointers instead of indexing. */
 
 #include <stdio.h>
 
-static int *day_table[] = 
-{
-    0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
-    0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-};
+static char row_0[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+static char row_1[] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+static char *day_table[] = {row_0, row_1};
 
 int day_of_year(int year, int day, int month);
 int month_day(int year, int year_day, int *pmonth, int *pday);
@@ -30,7 +29,7 @@ int day_of_year(int year, int day, int month)
     leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     for(i = 1; i < month; i++)
     {
-        day += day_table[leap][i];
+        day += *(*(day_table + leap) + i);
     }
     return day;
 }
@@ -46,9 +45,9 @@ int month_day(int year, int year_day, int *pmonth, int *pday)
     int i, leap;
 
     leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-    for (i = 1; year_day > day_table[leap][i]; i++)
+    for (i = 1; year_day > *(*(day_table + leap) + i); i++)
     {
-        year_day -= day_table[leap][i];
+        year_day -= *(*(day_table + leap) + i);
     }
     *pmonth = i;
     *pday = year_day;
