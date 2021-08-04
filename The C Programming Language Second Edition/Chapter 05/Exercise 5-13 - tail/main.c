a7b36bcb501410d5c754eddb71b301bfb111b18d
jamiesandell <jamie.sandell@outlook.com>
2021-08-04 07:50:25 +0100

    Exercise 5-13

--- The C Programming Language Second Edition/Chapter 05/Exercise 5-13 - tail/main.c
+++ The C Programming Language Second Edition/Chapter 05/Exercise 5-13 - tail/main.c
@@ -11,11 +11,84 @@ Section 5.6, not in a two-dimensional array of fixed size. */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_LINE_LENGTH 100
+#define MAX_LINE_LENGTH 1000 /* maximum characters (including the null terminator) for a line */
+#define MAX_LINES 5000 /* total number of lines allowed */
 
+#define ALLOC_SIZE 10000 /* size of available space */
+static char allocbuf[ALLOC_SIZE]; /* storage for alloc */
+static char *allocp = allocbuf; /* next free position */
+
+void afree(char *p);
+char *alloc(int n);
 int get_line(char line[], int max_line_length);
 void print_usage();
+int read_lines(char *lineptr[], int max_lines);
+void write_lines(char *lineptr[], int nlines);
+
+int main(int argc, char **argv)
+{
+    /* Process the command line */
+    if (argc > 2)
+    {
+        print_usage();
+        return -1;
+    }
+    int n = 10; /* default last n lines of input to print */
+    if (argc == 2)
+    {
+        /* Validate the arguments passed in */
+        if ((*++argv)[0] == '-')
+        {
+            n = atoi(*argv);
+            if (n == 0)
+            {
+                print_usage();
+                return -1;
+            }
+            if (n > MAX_LINES)
+            {
+                printf("-n cannot exceed %i\n", MAX_LINES);
+            }
+            n *= -1; /* get rid of the '-' character read in */
+        }
+        else
+        {
+            print_usage();
+            return -1;
+        }
+    }
+
+    char *lineptr[MAX_LINES]; /* pointers to text lines */
+    int nlines; /* number of input lines read */
+    if ((nlines = read_lines(lineptr, MAX_LINES)) >= 0)
+    {
+        write_lines(lineptr, (n > nlines ? n : nlines)); /* write the last n lines, unless lines read was smaller, then read write them instead */
+    }
+
+    return 0;
+}
+
+/* free storage pointed to by p */
+void afree(char *p)
+{
+    if (p >= allocbuf && p < allocbuf + ALLOC_SIZE)
+    {
+        allocp = p;
+    }
+}
+
+/* return pointer to n characters */
+char *alloc(int n)
+{
+    if (allocbuf + ALLOC_SIZE - allocp >= n) /* it fits */
+    {
+        allocp +=n;
+        return allocp - n; /* old p */
+    }
+    return 0; /* not enough room */
+}
 
 /* max_line_length is inclusive of the null terminator */
 int get_line(char line[], int max_line_length)
@@ -35,38 +108,32 @@ void print_usage()
     printf("Usage tail -n, e.g. tail -5\n");
 }
 
-int main(int argc, char **argv)
+int read_lines(char *lineptr[], int max_lines)
 {
-    /* Process the command line */
-    if (argc > 2)
+    int len, nlines;
+    char *p, line[MAX_LINE_LENGTH];
+
+    nlines = 0;
+    while((len = get_line(line, MAX_LINE_LENGTH)) > 0)
     {
-        print_usage();
-        return -1;
-    }
-    int n = 10;
-    if (argc == 2)
-    {
-        if ((*++argv)[0] == '-')
+        if (nlines >= max_lines || (p = alloc(len)) == NULL)
         {
-            n = atoi(*argv);
-            if (n == 0)
-            {
-                print_usage();
-                return -1;
-            }
-            n *= -1;
+            return -1; /* read too many lines or there was enough free memory */
         }
         else
         {
-            print_usage();
-            return -1;
+            line[len - 1] = '\0'; /* delete the newline character */
+            strcpy(p, line);
+            lineptr[nlines++] = p;
         }
     }
+    return nlines;
+}
 
-    char* lines[MAX_LINE_LENGTH * n];
-    char input[MAX_LINE_LENGTH];
-
-    while(get_line(input, MAX_LINE_LENGTH) > 0) 
-
-    return 0;
+void write_lines(char *lineptr[], int nlines)
+{
+    while (nlines-- > 0)
+    {
+        printf("%s\n", *lineptr++);
+    }
 }
