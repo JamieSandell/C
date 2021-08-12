ec6ac6ce22f5dc6f530716ab574524547a499e6d
Jamie Sandell <jamie.sandell@outlook.com>
2021-08-12 13:42:19 +0100

    Exercise 5-14

--- The C Programming Language Second Edition/Chapter 05/Exercise 5-14 - Sort/main.c
+++ The C Programming Language Second Edition/Chapter 05/Exercise 5-14 - Sort/main.c
@@ -4,6 +4,7 @@ Be sure that -r works with -n. */
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_LINE_LENGTH 100 /* max length of a line, including the null terminating character */
 #define MAX_LINES 5000 /* max #lines to be sorted */
 char *line_pointer[MAX_LINES]; /* pointers to text lines */
 
@@ -12,6 +13,12 @@ void writelines(char *line_pointer[], int number_of_lines);
 
 void qsort(void *line_pointer[], int left, int right, int (*comp)(void *, void *));
 int numcmp(char *, char *);
+
+#define ALLOC_SIZE 10000 /* size of available space */
+static char alloc_buffer[ALLOC_SIZE]; /* storage for alloc */
+static char *alloc_pointer = alloc_buffer; /* next free position */
+void afree(char *p);
+char *alloc(int size);
 
 int main(int argc, char *argv[])
 {
