2bd1843a03e0986b674b99ea9c2674f05e539722
jamiesandell <jamie.sandell@outlook.com>
2021-08-13 08:59:27 +0100

    Exercise 5-14

--- The C Programming Language Second Edition/Chapter 05/Exercise 5-14 - Sort/main.c
+++ The C Programming Language Second Edition/Chapter 05/Exercise 5-14 - Sort/main.c
@@ -7,7 +7,6 @@ Be sure that -r works with -n. */
 #define MAX_LINE_LENGTH 100 /* max length of a line, including the null terminating character */
 #define MAX_LINES 5000 /* max number of lines to be sorted */
 char *line_pointer[MAX_LINES]; /* pointers to text lines */
-
 int get_line(char line[]);
 int read_lines(char *line_pointer[], int number_of_lines);
 void write_lines(char *line_pointer[], int number_of_lines);
@@ -29,5 +28,42 @@ int main(int argc, char *argv[])
 int get_line(char line[])
 {
     int char_count = 0;
-    while 
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF  && char_count < MAX_LINE_LENGTH - 1)
+    {
+        line[char_count++] = c;
+    }
+    if (c = '\n')
+    {
+        line[char_count++] = '\n';
+    }
+    line[char_count] = '\0';
+    return char_count;
+}
+
+int read_lines(char *line_pointer[], int number_of_lines)
+{
+    int lines_read = 0;
+    char line[MAX_LINE_LENGTH];
+    char *p;
+    int line_length;
+    while ((line_length = get_line(line)) > 0 && lines_read < MAX_LINES)
+    {
+        if (lines_read >= MAX_LINES || (p = alloc(line_length)) == NULL)
+        {
+            return -1;
+        }
+        line[line_length - 1] = '\0'; /* delete the new line character */
+        strcpy(p, line); /* copy the string from line to where p points to in our memory buffer */
+        line_pointer[lines_read++] = p;
+    }
+    return lines_read;
+}
+
+void write_lines(char *line_pointer[], int number_of_lines)
+{
+    while (number_of_lines-- > 0)
+    {
+        printf("%s\n", *line_pointer++);
+    }
 }
