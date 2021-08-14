25963f383c0961d8ca8ee6bfe7b8a9541fd23ecf
jamiesandell <jamie.sandell@outlook.com>
2021-08-14 22:46:19 +0100

    Exercise 5-15

--- The C Programming Language Second Edition/Chapter 05/Exercise 5-15 - Sort/main.c
+++ The C Programming Language Second Edition/Chapter 05/Exercise 5-15 - Sort/main.c
@@ -16,8 +16,9 @@ void write_lines(char *line_pointer[], int number_of_lines);
 int reverse_compare(void *a, void *b);
 int (*base_compare)(void *, void *);
 int (*compare)(void *, void *);
-void my_qsort(void *line_pointer[], int left, int right, int (*comp)(void *, void *));
-int numcmp(char *, char *);
+void my_qsort(void *v[], int left, int right, int (*comp)(void *, void *));
+int numcmp(const char *s1, const char *s2);
+void swap(void *v[], int i, int j);
 
 #define ALLOC_SIZE 10000
 static char alloc_buffer[ALLOC_SIZE];
@@ -31,7 +32,7 @@ int main(int argc, char **argv)
     int numeric = 0;
     int reverse = 0;
     int ignore_case = 0;
-    while (--argc > 0 && (*++argv[0]) == '-') /* inc. argv, then dereference then get the first character */
+    while (--argc > 0 && (*++argv)[0] == '-') /* inc. argv, then dereference then get the first character */
     {
         while (c = *++argv[0])
         {
@@ -53,8 +54,17 @@ int main(int argc, char **argv)
             }
         }
     }
-
-    return 0;
+    int lines_read;
+    if ((lines_read = read_lines(line_pointer, MAX_LINES)) >= 0)
+    {
+        base_compare = (int (*)(void *, void*))(numeric ? numcmp : strcmp);
+        compare = (int (*)(void *, void*))(reverse ? reverse_compare : base_compare);
+        my_qsort((void **)line_pointer, 0, lines_read - 1, compare);
+        write_lines(line_pointer, lines_read);
+        return 0;
+    }
+    printf("Error: Input too big to sort.");
+    return -1;
 }
 
 int get_line(char line[], int max_line_length)
@@ -77,15 +87,100 @@ int get_line(char line[], int max_line_length)
     Returns -1 if lines read exceeds max_number_of_lines or not enough free memory in our buffer. */
 int read_lines(char *line_pointer[], int max_number_of_lines)
 {
-    char line[MAX_LINES];
+    char line[MAX_LINE_LENGTH];
     int lines_read = 0;
-    int line_length = 0;
+    int line_length;
     char *p;
-    while ((line_length = get_line(line, max_number_of_lines)) > 0 )
+    while ((line_length = get_line(line, MAX_LINE_LENGTH)) > 0 )
     {
         if (lines_read >= max_number_of_lines || (p = alloc(line_length)) == NULL)
         {
             return -1;
         }
+        line[line_length - 1] = '\0'; /* delete the new line character */
+        strcpy(p, line);
+        line_pointer[lines_read++] = p;
+    }
+    return lines_read;
+}
+
+void write_lines(char *line_pointer[], int number_of_lines)
+{
+    while (number_of_lines-- > 0)
+    {
+        printf("%s\n", *line_pointer++);
+    }
+}
+
+int reverse_compare(void *a, void *b)
+{
+    return (*base_compare)(b, a);
+}
+
+void my_qsort(void *v[], int left, int right, int (*comp)(void *, void*))
+{
+    int i, last;
+
+    if (left >= right) /* do nothing if array contains fewer than two elements */
+    {
+        return;
+    }
+    swap(v, left, (left + right) / 2);
+    last = left;
+    for (i = left + 1; i <= right; i++)
+    {
+        if ((*comp)(v[i], v[left]) < 0)
+        {
+            swap(v, ++last, i);
+        }
+    }
+    swap(v, left, last);
+    my_qsort(v, left, last - 1, comp);
+    my_qsort(v, last + 1, right, comp);
+}
+
+/* s1 < s2 == return -1 
+    s1 > s2 == return 1
+    return 0*/
+int numcmp(const char *s1, const char *s2)
+{
+    double v1, v2;
+
+    v1 = atof(s1);
+    v2 = atof(s2);
+    if (v1 < v2)
+    {
+        return -1;
+    }
+    else if (v1 > v2)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void swap(void *v[], int i, int j)
+{
+    void *temp;
+
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+void afree(char *p)
+{
+    if (p >= alloc_buffer && p < alloc_buffer + ALLOC_SIZE)
+    {
+        alloc_pointer = p;
+    }
+}
+
+char *alloc(int size)
+{
+    if (alloc_pointer + size <= alloc_buffer + ALLOC_SIZE) /* Does the request fit? */
+    {
+        alloc_pointer += size;   /* Point the buffer pointer to the next free space */
+        return alloc_pointer - size; /* Return the old buffer pointer position */
     }
 }
