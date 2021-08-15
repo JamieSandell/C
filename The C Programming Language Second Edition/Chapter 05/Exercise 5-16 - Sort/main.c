f69ac10fdc661884bd81a7ca826b3dfcca9d9c4a
jamiesandell <jamie.sandell@outlook.com>
2021-08-15 11:57:46 +0100

    Exercise 5-16

--- The C Programming Language Second Edition/Chapter 05/Exercise 5-16 - Sort/main.c
+++ The C Programming Language Second Edition/Chapter 05/Exercise 5-16 - Sort/main.c
@@ -9,7 +9,11 @@
 #define MAX_LINE_LENGTH 100;
 #define MAX_LINES 5000 /* Max number of lines to be sorted. */
 
+
 char *line_pointer[MAX_LINES]; /* pointers to text lines */
+
+int (*base_compare)(void *a, void *b); /* our function pointers to comparison functions */
+int (*compare)(void *a, void *b);
 
 int get_line(char line[], int line_size);
 int read_lines(char *line_pointer[], int max_number_of_lines);
@@ -17,6 +21,7 @@ void write_lines(char* line_pointer[], int number_of_lines);
 
 void my_qsort(void *v[], int left, int right, int (*comp)(void *, void*));
 int numcmp(const char *s1, const char *s2);
+int reverse_cmp(void *a, void *b);
 int str_case_cmp(const char *s1, const char *s2);
 
 int main(int argc, char **argv)
