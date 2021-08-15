d9fa9a638626979928b45aaa180fb73632b7d27a
jamiesandell <jamie.sandell@outlook.com>
2021-08-15 09:45:50 +0100

    Exercise 5-16

---
+++ The C Programming Language Second Edition/Chapter 05/Exercise 5-16 - Sort/main.c
@@ -0,0 +1,25 @@
+/* Add the -d ("directory order") option, which makes comparisons only on letters, numbers and blanks.
+    Make sure it works in conjunction with -f */
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 100;
+#define MAX_LINES 5000 /* Max number of lines to be sorted. */
+
+char *line_pointer[MAX_LINES]; /* pointers to text lines */
+
+int get_line(char line[], int line_size);
+int read_lines(char *line_pointer[], int max_number_of_lines);
+void write_lines(char* line_pointer[], int number_of_lines);
+
+void my_qsort(void *v[], int left, int right, int (*comp)(void *, void*));
+int numcmp(const char *s1, const char *s2);
+int str_case_cmp(const char *s1, const char *s2);
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
