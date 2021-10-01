957ad323d43b4fee7b5f27514e12be6f2f018076
jamiesandell <jamie.sandell@outlook.com>
2021-10-01 07:33:48 +0100

    Exercise 6-3

---
+++ The C Programming Language Second Edition/Chapter 06/Exercise 6-3 - cross-referencer/main.c
@@ -0,0 +1,3 @@
+/* Write a cross-referencer that prints a list of all words in a document,
+and, for each word, a list of the line numbers on which it occurs. Remove
+noise words like "the," "and," and so on */
