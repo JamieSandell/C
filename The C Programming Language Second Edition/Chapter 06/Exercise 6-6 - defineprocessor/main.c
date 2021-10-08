aae2a87d7546362f1e476dbb192c9868d1a834bf
jamiesandell <jamie.sandell@outlook.com>
2021-10-08 13:24:27 +0100

    Exercise 6-6

--- The C Programming Language Second Edition/Chapter 06/Exercise 6-6 - defineprocessor/main.c
+++ The C Programming Language Second Edition/Chapter 06/Exercise 6-6 - defineprocessor/main.c
@@ -1,6 +1,7 @@
 /* Implement a simple version of the #define processor (i.e., no arguments) suitable for use with C programs,
 based on the routines of this section. You may also find getch and ungetch useful. */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +9,7 @@ based on the routines of this section. You may also find getch and ungetch usefu
 #define BUF_SIZE 100
 #define HASH_SIZE 101
 #define MAX_WORD 100
+#define WORD_SIZE 100
 
 struct nlist /* table entry: */
 {
@@ -21,6 +23,7 @@ static int bufp = 0; /* next free position in buf */
 static struct nlist *hashtab[HASH_SIZE]; /* pointer table */
 
 int getch(void);
+int get_number(void);
 int get_word(char *word, unsigned limit);
 unsigned hash(const char *s);
 struct nlist *install(const char *name, const char *defn);
@@ -29,6 +32,30 @@ void ungetch(int c);
 
 int main()
 {
+    enum state {unprocessed, define};
+    enum state state = unprocessed;
+
+    char word[WORD_SIZE];
+    int c;
+    while ((c = getch()) != '#' || c != EOF)
+    {
+        switch (state)
+        {
+            case unprocessed:
+                c = get_word(word, MAX_WORD);
+                if (word == "define")
+                {
+                    state = define;
+                }
+                break;
+            case define:
+
+                break;
+            default:
+                printf("Error: unhandled switch statement in main()\n");
+                return -1;
+        }
+    }
     return 0;
 }
 
@@ -38,11 +65,41 @@ int getch(void)
     return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
+/* get_number: get net number from input */
+int get_number(void)
+{
+    
+}
+
 /* get_word: get next word or character from input */
 int get_word(char *word, unsigned limit)
 {
     int c;
     char *w = word;
+
+    while (isspace(c = getch()))
+    {
+        ;
+    }
+    if (c != EOF)
+    {
+        *w++ = c;
+    }
+    if (!isalpha(c))
+    {
+        *w = '\0';
+        return c;
+    }
+    for (; --limit > 0; ++w)
+    {
+        if (!isalnum(*w = getch()))
+        {
+            ungetch(*w);
+            break;
+        }
+    }
+    *w = '\0';
+    return word[0];
 }
 
 /* hash: form hash value for string s */
