7621f88c6dd7ab007d2637acb38fcc9425b53441
jamiesandell <jamie.sandell@outlook.com>
2021-10-17 11:50:43 +0100

    Exercise 7-7

--- The C Programming Language Second Edition/Chapter 07/Exercise 7-7 - patternfinder/main.c
+++ The C Programming Language Second Edition/Chapter 07/Exercise 7-7 - patternfinder/main.c
@@ -1,14 +1,22 @@
 /* Modify the pattern finding program of Chapter 5 to take its input from a set of named files or,
 if no files are named as arguments, from the standard input. Should the file name be printed when a matching line is found? 
 
-patternfinder [optional]-x [optional]-n [required]pattern [optional]file1.txt...[optional]fileN.txt */
+patternfinder [optional]-x [optional]-n [required]-ppattern [optional]file1.txt...[optional]fileN.txt */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MAX_FILES 100
+#define MAX_ARGS MAX_FILES + 3
 #define MAX_FILE_PATH 100
+#define MAX_PATTERN 100
+
+struct flags
+{
+    unsigned int exclusion          : 1;
+    unsigned int print_line_number  : 1;
+} flags;
 
 struct my_file
 {
@@ -16,15 +24,90 @@ struct my_file
     char file_path[MAX_FILE_PATH];
 };
 
+struct my_file *create_my_file(FILE *fp, const char *file_path);
 int get_line(char *line, unsigned int max, FILE *fp);
 FILE *open_file(const char *file_path, const char *mode, const char *program_name);
 
 int main(int argc, char *argv[])
 {
-    struct my_file files[MAX_FILES] = {0};
-    unsigned int files_index = 0;
+    if (argc > MAX_ARGS)
+    {
+        fprintf(stderr, "Too many arguments, max arguments: %i\n", MAX_ARGS);
+        exit(-1);
+    }
+
+    char pattern[MAX_PATTERN];
+    if (argc == 1) /* No cmd flags specified */
+    {
+        printf("Please enter the pattern to find:\t");
+        get_line(pattern, MAX_PATTERN, stdin);
+    }
+    else /* Process the cmd flags */
+    {
+        struct my_file *files[MAX_FILES];
+        unsigned int files_index = 0;
+        char *c;
+        while (--argc > 0)
+        {
+            c = *++argv;
+            if (*c == '-') /* not a file */
+            {
+                switch (*++c)
+                {
+                    case 'x':
+                        flags.exclusion = 1;
+                        break;
+                    case 'n':
+                        flags.print_line_number = 1;
+                        break;
+                    case 'p':
+                        strcpy(pattern, ++c);
+                        break;
+                    default:
+                        fprintf(stderr, "Error: unknown argument %c\n", *c);
+                        exit(-1);
+                }
+            }
+            else /* file */
+            {
+                FILE *fp = fopen(c, "r");
+                if (fp == NULL)
+                {
+                    fprintf(stderr, "Error: %s failed to open the file: %s\n", argv[0], c);
+                    exit(-1);
+                }
+                files[files_index] = create_my_file(fp, c);
+                if (files[files_index] == NULL)
+                {
+                    fprintf(stderr, "failed to create my_file from file: %s\n", c);
+                }
+                ++files_index;
+            }       
+        }
+        for (unsigned int i = 0; i < files_index; ++i)
+        {
+            if (!flags.exclusion)
+            {
+                if (strstr(files[i].))
+            }
+        }
+    }
 
     return 0;
+}
+
+struct my_file *create_my_file(FILE *fp, const char *file_path)
+{
+    struct my_file *file;
+
+    file = malloc(sizeof(*file));
+    if (file == NULL)
+    {
+        return NULL;
+    }
+    strcpy(file->file_path, file_path);
+    file->fp = fp;
+    return file;
 }
 
 int get_line(char *line, unsigned int max, FILE *fp)
