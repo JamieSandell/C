8f8975d9909d6e182b4746526018ddcfc88ebb02
jamiesandell <jamie.sandell@outlook.com>
2021-10-24 09:00:43 +0100

    Exercise 8-5

--- The C Programming Language Second Edition/Chapter 08/Exercise 8-5 - fsize/main.c
+++ The C Programming Language Second Edition/Chapter 08/Exercise 8-5 - fsize/main.c
@@ -1,9 +1,4 @@
-/* The standard library function
-    int fseek(FILE *fp, long offset, int origin)
-is identical to lseek except that fp is a file pointer instead of a file descriptor
-and the return value is an int status, not a position. Write fseek. Make sure
-that your fseek coordinates properly with the buffering done for the other
-functions of the library. */
+/* Modify the fsize program to to print the other information contained in the inode entry. */
 
 #define _DEFAULT_SOURCE
 #include <dirent.h>
@@ -13,16 +8,17 @@ functions of the library. */
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-/*#include "dirent.h"*/
+#include <time.h>
 
-void fsize(char *);
+char *format_date(char *str, time_t val);
+void fsize(char *name);
 
 /* print file sizes */
 int main(int argc, char *argv[])
 {
     if (argc == 1)
     {
-        fsize("."); /* defaulr: current directory */
+        fsize("."); /* default: current directory */
     }
     else
     {
@@ -34,7 +30,14 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void dirwalk(char *, void (*fcn)(char *));
+/* https://stackoverflow.com/questions/26306644/how-to-display-st-atime-and-st-mtime/26307281 */
+char *format_date(char *str, time_t val)
+{
+    strftime(str, 36, "%Y.%m.%d %H:%M:%S", localtime(&val));
+    return str;
+}
+
+void dirwalk(char *dir, void (*fcn)(char *));
 
 /* fsize: print size of file "name" */
 void fsize(char *name)
@@ -50,7 +53,22 @@ void fsize(char *name)
     {
         dirwalk(name, fsize);
     }
-    printf("%8ld %s\n", stbuf.st_size, name);
+    printf("File: %s\n", name);
+    printf("--------------------------------\n");
+    printf("%-40s%20lu\n", "ID of device containing file:", stbuf.st_dev);
+    printf("%-40s%20lu\n", "Inode number:", stbuf.st_ino);
+    printf("%-40s%20u\n", "File type and mode:", stbuf.st_mode);
+    printf("%-40s%20u\n", "Number of hard links:", stbuf.st_nlink);
+    printf("%-40s%20u\n", "User ID of owner", stbuf.st_uid);
+    printf("%-40s%20u\n", "Group of ID owner:", stbuf.st_gid);
+    printf("%-40s%20lu\n", "Device ID (if special file):", stbuf.st_rdev);
+    printf("%-40s%20ld\n", "Total size in bytes:", stbuf.st_size);
+    printf("%-40s%20ld\n", "Block size for file system I/O:", stbuf.st_blksize);
+    printf("%-40s%20ld\n", "Number of 512B blocks allocated:", stbuf.st_blocks);
+    char date[36];
+    printf("%-40s%20s\n", "Access:", format_date(date, stbuf.st_atime));
+    printf("%-40s%20s\n", "Modify:", format_date(date, stbuf.st_mtime));
+    printf("%-40s%20s\n", "Change:", format_date(date, stbuf.st_ctime));
 }
 
 #define MAX_PATH 1024
